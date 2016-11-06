54a3be7ad4eea5bf54aa0bef7b0fcb5a1953edcf
Rosalie Tolentino <rosatolen@thoughtworks.com>
2016-11-06 16:22:57 -0500

    Renames AddContact2 to SMP Wizard

---
+++ gui/definitions/smp_wizard.go
@@ -0,0 +1,96 @@
+package definitions
+
+func init() {
+	add(`SMPWizard`, &defSMPWizard{})
+}
+
+type defSMPWizard struct{}
+
+func (*defSMPWizard) String() string {
+	return `<interface>
+   <object class="GtkAssistant" id="SMPWizard">
+    <signal name="close" handler="on_close_signal" />
+    <signal name="cancel" handler="on_cancel_signal" />
+    <signal name="escape" handler="on_escape_signal" />
+    <child>
+        <object class="GtkLabel" id="label1">
+            <property name="visible">True</property>
+            <property name="label" translatable="yes">Make sure there is no one else reading your messages.</property>
+        </object>
+        <packing>
+            <property name="page_type">content</property>
+        </packing>
+    </child>
+    <child>
+        <object class="GtkBox">
+        <property name="homogeneous">false</property>
+        <property name="orientation">GTK_ORIENTATION_VERTICAL</property>
+        <property name="spacing">6</property>
+        <child>
+            <object class="GtkLabel">
+                <property name="visible">True</property>
+                <property name="label" translatable="yes">Share this pin only with your contact.</property>
+            </object>
+        </child>
+        <child>
+            <object class="GtkLabel">
+                <property name="visible">True</property>
+                <property name="label" translatable="yes">It can only be used once.</property>
+            </object>
+        </child>
+        <child>
+            <object class="GtkLabel" id="PinLabel">
+                <property name="visible">True</property>
+                <property name="selectable">True</property>
+                <property name="label">placeholder</property>
+            </object>
+        </child>
+        <child>
+            <object class="GtkGrid">
+            <child>
+                <object class="GtkLabel">
+                    <property name="visible">True</property>
+                    <property name="label" translatable="yes">Share in person</property>
+                    <property name="justify">GTK_JUSTIFY_LEFT</property>
+                    <property name="halign">GTK_ALIGN_START</property>
+                </object>
+                <packing>
+                    <property name="left-attach">1</property>
+                    <property name="top-attach">0</property>
+                </packing>
+            </child>
+            <child>
+                <object class="GtkLabel">
+                    <property name="visible">True</property>
+                    <property name="label" translatable="yes">Share through an encrypted channel</property>
+                    <property name="justify">GTK_JUSTIFY_LEFT</property>
+                    <property name="halign">GTK_ALIGN_START</property>
+                </object>
+                <packing>
+                    <property name="left-attach">1</property>
+                    <property name="top-attach">1</property>
+                </packing>
+            </child>
+            <child>
+                <object class="GtkLabel">
+                    <property name="visible">True</property>
+                    <property name="label" translatable="yes">Share in a phone call</property>
+                    <property name="justify">GTK_JUSTIFY_LEFT</property>
+                    <property name="halign">GTK_ALIGN_START</property>
+                </object>
+                <packing>
+                    <property name="left-attach">1</property>
+                    <property name="top-attach">2</property>
+                </packing>
+            </child>
+            </object>
+        </child>
+        </object>
+        <packing>
+            <property name="page_type">summary</property>
+        </packing>
+    </child>
+  </object>
+</interface>
+`
+}
