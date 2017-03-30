6a975688f3e061b26a45c693f2b0d023d695ff91
Rosalie Tolentino <rtolenti@thoughtworks.com>
2017-03-30 10:29:22 -0500

    Adds XML changes

--- gui/definitions/smp_wizard.go
+++ gui/definitions/smp_wizard.go
@@ -8,145 +8,133 @@ type defSMPWizard struct{}
 
 func (*defSMPWizard) String() string {
 	return `<interface>
-   <object class="GtkAssistant" id="SMPWizard">
-    <signal name="close" handler="on_close_signal" />
-    <signal name="cancel" handler="on_cancel_signal" />
-    <signal name="escape" handler="on_escape_signal" />
-    <child>
-        <object class="GtkBox">
-            <property name="homogeneous">false</property>
-            <property name="orientation">GTK_ORIENTATION_VERTICAL</property>
-            <property name="spacing">18</property>
-            <child>
-                <object class="GtkImage">
-                    <property name="file">build/images/maninthemiddle.png</property>
+  <object class="GtkDialog" id="dialog">
+    <property name="window-position">GTK_WIN_POS_CENTER</property>
+    <child internal-child="vbox">
+        <object class="GtkBox" id="box">
+          <property name="border-width">10</property>
+          <property name="homogeneous">false</property>
+          <property name="orientation">GTK_ORIENTATION_VERTICAL</property>
+          <child>
+            <object class="GtkBox">
+              <property name="homogeneous">false</property>
+              <property name="orientation">GTK_ORIENTATION_VERTICAL</property>
+              <property name="spacing">6</property>
+              <child>
+                  <object class="GtkImage">
+                      <property name="file">build/images/Pin_1.png</property>
+                  </object>
+              </child>
+              <child>
+                  <object class="GtkLabel">
+                      <property name="label" translatable="yes">Share the one time PIN below with this contact and remember it to validate your connection later.</property>
+                  </object>
+              </child>
+              <child>
+                  <object class="GtkLabel">
+                      <property name="label" translatable="yes">You will be prompted for the PIN after a conversation with them has started.</property>
+                  </object>
+              </child>
+              <child>
+                  <object class="GtkLabel" id="PinLabel">
+                      <property name="visible">True</property>
+                      <property name="selectable">True</property>
+                  </object>
+              </child>
+              <child>
+                  <object class="GtkButton" id="GenPin">
+                      <property name="label" translatable="yes">Generate New PIN</property>
+                      <property name="receives-default">True</property>
+                      <signal name="clicked" handler="on_gen_pin"/>
+                  </object>
+                  <packing>
+                      <property name="padding">10</property>
+                  </packing>
+              </child>
+              <child>
+                  <object class="GtkGrid">
+                  <property name="column-spacing">6</property>
+                  <property name="row-spacing">2</property>
+                  <child>
+                      <object class="GtkImage">
+                          <property name="file">build/images/padlock.png</property>
+                      </object>
+                      <packing>
+                          <property name="left-attach">0</property>
+                          <property name="top-attach">0</property>
+                      </packing>
+                  </child>
+                  <child>
+                      <object class="GtkLabel">
+                          <property name="visible">True</property>
+                          <property name="label" translatable="yes">Share in person</property>
+                          <property name="justify">GTK_JUSTIFY_LEFT</property>
+                          <property name="halign">GTK_ALIGN_START</property>
+                      </object>
+                      <packing>
+                          <property name="left-attach">1</property>
+                          <property name="top-attach">0</property>
+                      </packing>
+                  </child>
+                  <child>
+                      <object class="GtkImage">
+                          <property name="file">build/images/padlock.png</property>
+                      </object>
+                      <packing>
+                          <property name="left-attach">0</property>
+                          <property name="top-attach">1</property>
+                      </packing>
+                  </child>
+                  <child>
+                      <object class="GtkLabel">
+                          <property name="visible">True</property>
+                          <property name="label" translatable="yes">Share through an encrypted channel</property>
+                          <property name="justify">GTK_JUSTIFY_LEFT</property>
+                          <property name="halign">GTK_ALIGN_START</property>
+                      </object>
+                      <packing>
+                          <property name="left-attach">1</property>
+                          <property name="top-attach">1</property>
+                      </packing>
+                  </child>
+                  <child>
+                      <object class="GtkImage">
+                          <property name="file">build/images/alert.png</property>
+                      </object>
+                      <packing>
+                          <property name="left-attach">0</property>
+                          <property name="top-attach">2</property>
+                      </packing>
+                  </child>
+                  <child>
+                      <object class="GtkLabel">
+                          <property name="visible">True</property>
+                          <property name="label" translatable="yes">Share in a phone call</property>
+                          <property name="justify">GTK_JUSTIFY_LEFT</property>
+                          <property name="halign">GTK_ALIGN_START</property>
+                      </object>
+                      <packing>
+                          <property name="left-attach">1</property>
+                          <property name="top-attach">2</property>
+                      </packing>
+                  </child>
+                  </object>
+              </child>
+            </object>
+          </child>
+          <child internal-child="action_area">
+            <object class="GtkButtonBox" id="button_box">
+              <property name="orientation">GTK_ORIENTATION_HORIZONTAL</property>
+              <child>
+                <object class="GtkButton" id="button_finished">
+                  <property name="can-default">true</property>
+                  <property name="label" translatable="yes">Finished</property>
+                  <signal name="clicked" handler="close_share_pin"/>
                 </object>
-                <packing>
-                    <property name="padding">60</property>
-                </packing>
-            </child>
-            <child>
-                <object class="GtkLabel">
-                    <property name="visible">True</property>
-                    <property name="label" translatable="yes">Make sure there is no one else reading your messages.</property>
-                </object>
-            </child>
+              </child>
+            </object>
+          </child>
         </object>
-        <packing>
-            <property name="page_type">content</property>
-        </packing>
-    </child>
-    <child>
-        <object class="GtkBox">
-        <property name="homogeneous">false</property>
-        <property name="orientation">GTK_ORIENTATION_VERTICAL</property>
-        <property name="spacing">6</property>
-        <child>
-            <object class="GtkImage">
-                <property name="file">build/images/Pin_1.png</property>
-            </object>
-        </child>
-        <child>
-            <object class="GtkLabel">
-                <property name="visible">True</property>
-                <property name="label" translatable="yes">Share this pin only with your contact.</property>
-            </object>
-        </child>
-        <child>
-            <object class="GtkLabel">
-                <property name="visible">True</property>
-                <property name="label" translatable="yes">It can only be used once.</property>
-            </object>
-        </child>
-        <child>
-            <object class="GtkLabel" id="PinLabel">
-                <property name="visible">True</property>
-                <property name="selectable">True</property>
-            </object>
-        </child>
-        <child>
-            <object class="GtkButton" id="GenPin">
-                <property name="label" translatable="yes">Generate New PIN</property>
-                <property name="receives-default">True</property>
-                <signal name="clicked" handler="on_gen_pin"/>
-            </object>
-            <packing>
-                <property name="padding">10</property>
-            </packing>
-        </child>
-        <child>
-            <object class="GtkGrid">
-            <property name="column-spacing">6</property>
-            <property name="row-spacing">2</property>
-            <child>
-                <object class="GtkImage">
-                    <property name="file">build/images/padlock.png</property>
-                </object>
-                <packing>
-                    <property name="left-attach">0</property>
-                    <property name="top-attach">0</property>
-                </packing>
-            </child>
-            <child>
-                <object class="GtkLabel">
-                    <property name="visible">True</property>
-                    <property name="label" translatable="yes">Share in person</property>
-                    <property name="justify">GTK_JUSTIFY_LEFT</property>
-                    <property name="halign">GTK_ALIGN_START</property>
-                </object>
-                <packing>
-                    <property name="left-attach">1</property>
-                    <property name="top-attach">0</property>
-                </packing>
-            </child>
-            <child>
-                <object class="GtkImage">
-                    <property name="file">build/images/padlock.png</property>
-                </object>
-                <packing>
-                    <property name="left-attach">0</property>
-                    <property name="top-attach">1</property>
-                </packing>
-            </child>
-            <child>
-                <object class="GtkLabel">
-                    <property name="visible">True</property>
-                    <property name="label" translatable="yes">Share through an encrypted channel</property>
-                    <property name="justify">GTK_JUSTIFY_LEFT</property>
-                    <property name="halign">GTK_ALIGN_START</property>
-                </object>
-                <packing>
-                    <property name="left-attach">1</property>
-                    <property name="top-attach">1</property>
-                </packing>
-            </child>
-            <child>
-                <object class="GtkImage">
-                    <property name="file">build/images/alert.png</property>
-                </object>
-                <packing>
-                    <property name="left-attach">0</property>
-                    <property name="top-attach">2</property>
-                </packing>
-            </child>
-            <child>
-                <object class="GtkLabel">
-                    <property name="visible">True</property>
-                    <property name="label" translatable="yes">Share in a phone call</property>
-                    <property name="justify">GTK_JUSTIFY_LEFT</property>
-                    <property name="halign">GTK_ALIGN_START</property>
-                </object>
-                <packing>
-                    <property name="left-attach">1</property>
-                    <property name="top-attach">2</property>
-                </packing>
-            </child>
-            </object>
-        </child>
-        </object>
-        <packing>
-            <property name="page_type">summary</property>
-        </packing>
     </child>
   </object>
 </interface>
